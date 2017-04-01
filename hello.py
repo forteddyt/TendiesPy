76092910e3457855f4ef96d1a1036d5c5fb023f8
forteddyt <forteddyt@gmail.com>
2017-03-31 22:56:57 -0400

    Input field double-text bug fix

--- hello.py
+++ hello.py
@@ -1,10 +1,26 @@
-from flask import Flask, url_for
+from flask import Flask, url_for, session, request, redirect
 from flask import render_template
 app = Flask(__name__)
 
 @app.route('/')
 def main():
-    return render_template('index.html')
+	return render_template('index.html')
+
+def getFilters():
+	return redirect('/')
+
+@app.route('/test', methods=['POST', 'GET'])
+def addRegion():
+	try:
+		f = request.form
+		print(f)
+		for key in f.keys():
+			print(key + '\n')
+	except Exception as e:
+		print(Error)
+		pass
+	return redirect('/')
 
 if __name__ == '__main__':
-    app.run()
+	app.debug = True
+	app.run()
