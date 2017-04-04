0fdee1ff507d20de224ec24d59c38b619c904e23
forteddyt <forteddyt@gmail.com>
2017-04-03 22:00:05 -0400

    revert back to my code

--- application.py
+++ application.py
@@ -1,34 +1,83 @@
-from flask import Flask
+from flask import Flask, url_for, session, request, redirect, jsonify
+from flask import render_template
+import waiter
+import json
 
-# print a nice greeting.
-def say_hello(username = "World"):
-    return '<p>Hello %s!</p>\n' % username
-
-# some bits of text for the page.
-header_text = '''
-    <html>\n<head> <title>EB Flask Test</title> </head>\n<body>'''
-instructions = '''
-    <p><em>Hint</em>: This is a RESTful web service! Append a username
-    to the URL (for example: <code>/Thelonious</code>) to say hello to
-    someone specific.</p>\n'''
-home_link = '<p><a href="/">Back</a></p>\n'
-footer_text = '</body>\n</html>'
-
-# EB looks for an 'application' callable by default.
 application = Flask(__name__)
 
-# add a rule for the index page.
-application.add_url_rule('/', 'index', (lambda: header_text +
-    say_hello() + instructions + footer_text))
+@application.route('/')
+def main():
+	print("HELLO FORTEDDYT")
+	return render_template('index.html')
 
-# add a rule when the page is accessed with a name appended to the site
-# URL.
-application.add_url_rule('/<username>', 'hello', (lambda username:
-    header_text + say_hello(username) + home_link + footer_text))
+def getFilters():
+	return redirect('/')
 
-# run the app.
-if __name__ == "__main__":
-    # Setting debug to True enables debug output. This line should be
-    # removed before deploying a production app.
-    application.debug = True
-    application.run()
+@application.route('/_createPanels', methods=['GET'])
+def createPanels():
+	waiter.greet()
+	serializedForm = request.args.get('formData', 0)
+
+	myForm = serializeToDict(serializedForm)
+
+	# return jsonify(result="Test")
+
+	# print("itemList")
+	# print(itemList)
+
+	# print("jsonData")
+	# print(jsonData)
+
+	# jsonObj = {"attributes" : myForm["attributes"],
+	# 			"categories" : myForm["categories"],
+	# 			"location" : myForm["location"],
+	# 			"open_now" : myForm["open_now"],
+	# 			"price" : myForm["price"]}
+
+	# print(myForm)
+	# print("Is the above a JSON? -> " + str(isJsonable(myForm)))
+	results = waiter.reserveTable(myForm)
+
+	return jsonify(results)
+
+def serializeToDict(serializedForm):
+	print("Converting form to dict")
+	requiredKeys = ["attributes", "categories", "location", "open_now", "price", "sort_by"]
+
+	myForm = {}
+
+	for item in serializedForm.split('&'):
+		itemContents = item.split('=')
+
+		key = itemContents[0]
+		value = itemContents[1]
+
+		# print(key + " -> " + value)
+
+		if myForm.get(key) != None:
+			myForm[key].applicationend(value)
+		else:
+			if key == "radius" or key == "open_now":
+				temp = value
+			else:
+				temp = [value]
+
+			myForm[key] = temp
+
+	for key in requiredKeys:
+		if myForm.get(key) == None:
+			myForm[key] = ""
+
+	print(myForm)
+	return myForm
+
+def isJsonable(x):
+	try:
+		json.dumps(x)
+		return True
+	except:
+		return False
+
+if __name__ == '__main__':
+	application.debug = True
+	application.run()
